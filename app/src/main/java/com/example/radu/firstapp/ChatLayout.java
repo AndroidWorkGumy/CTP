bb9e8c4c7d381a1891bfd650c7a263dc2dca5a9d
AndroidWorkGrumy <radu_g1997@yahoo.com>
2016-02-01 21:12:56 +0200

    vrajeli what the fuck

--- app/src/main/java/com/example/radu/firstapp/ChatLayout.java
+++ app/src/main/java/com/example/radu/firstapp/ChatLayout.java
@@ -1,13 +1,41 @@
 package com.example.radu.firstapp;
 
+import android.app.Activity;
+import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class ChatLayout extends ActionBarActivity {
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class ChatLayout extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_layout);
+
+        Calendar calendar = Calendar.getInstance();
+        TextView dateAndHour = (TextView) findViewById(R.id.dateAndHourTextField);
+        TextView image1 = (TextView) findViewById(R.id.imageView1);
+        TextView image2 = (TextView) findViewById(R.id.imageView2);
+
+        dateAndHour.setText(MainActivity.getDay(calendar) + "/" + MainActivity.getMonth(calendar)
+         + "/" + calendar.get(Calendar.YEAR) + "  " + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE));
+
+        image1.setGravity(Gravity.CENTER);
+        image1.setBackgroundResource(R.drawable.sms_galben);
+        image1.setText(MainActivity.linie);
+        image1.setWidth(MainActivity.linie.length() * 5);
+
+        image2.setGravity(Gravity.CENTER);
+        image2.setBackgroundResource(R.drawable.sms_albastru);
+        image2.setText(MainActivity.text);
     }
 }
--- app/src/main/java/com/example/radu/firstapp/MainActivity.java
+++ app/src/main/java/com/example/radu/firstapp/MainActivity.java
@@ -18,14 +18,17 @@ import java.util.Random;
 
 public class MainActivity extends Activity {
 
-    private String getMinutes(Calendar rightNow) {
+    public static String text;
+    public static String linie;
+
+    public static String getMinutes(Calendar rightNow) {
 
         if (rightNow.get(Calendar.MINUTE) + 45 > 59)
             return (String.valueOf(rightNow.get(Calendar.MINUTE) - 15));
         return String.valueOf(rightNow.get(Calendar.MINUTE) + 45);
     }
 
-    private String getHourAndMinutes(Calendar rightNow) {
+    public static String getHourAndMinutes(Calendar rightNow) {
         if (!String.valueOf(rightNow.get((Calendar.MINUTE)) + 45).equals(getMinutes(rightNow))) {
             if (rightNow.get(Calendar.HOUR_OF_DAY) + 1 < 10)
                 return "0" + String.valueOf(rightNow.get(Calendar.HOUR_OF_DAY) + 1)
@@ -39,19 +42,19 @@ public class MainActivity extends Activity {
         return getHour(rightNow) + ":" + getMinutes(rightNow);
     }
 
-    private String getHour(Calendar rightNow) {
+    public static String getHour(Calendar rightNow) {
         if (rightNow.get(Calendar.HOUR_OF_DAY) < 10)
             return "0" + String.valueOf(rightNow.get(Calendar.HOUR_OF_DAY));
         return String.valueOf(rightNow.get(Calendar.HOUR_OF_DAY));
     }
 
-    private String getMonth(Calendar rightNow) {
+    public static String getMonth(Calendar rightNow) {
         if (rightNow.get(Calendar.MONTH) + 1 < 10)
             return "0" + String.valueOf(rightNow.get(Calendar.MONTH) + 1);
         return String.valueOf(rightNow.get(Calendar.MONTH) + 1);
     }
 
-    private String getDay(Calendar rightNow) {
+    public static String getDay(Calendar rightNow) {
         if (rightNow.get(Calendar.DAY_OF_MONTH) < 10)
             return "0" + String.valueOf(rightNow.get(Calendar.DAY_OF_MONTH));
         return String.valueOf(rightNow.get(Calendar.DAY_OF_MONTH));
@@ -63,27 +66,29 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         final Button button = (Button) findViewById(R.id.button);
         final EditText ctpLine = (EditText) findViewById(R.id.ctpLine);
+        final Intent intent = new Intent(MainActivity.this, ChatLayout.class);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int min = 132795;
                 int max = 865742;
                 Random randomNumber = new Random();
-                String linie = ctpLine.getText().toString();
+                linie = ctpLine.getText().toString();
                 if (!linie.isEmpty()) {
                     Calendar rightNow = Calendar.getInstance();
 
-                    String text = "Biletul pentru linia " + linie + " a fost activat. Valabil pana la " +
+                    text = "Biletul pentru linia " + linie + " a fost activat. Valabil pana la " +
                            getHourAndMinutes(rightNow)
                             + " in " + getDay(rightNow) + "/" + getMonth(rightNow) +
                             "/" + rightNow.get(Calendar.YEAR) +
-                            ". Cost total:0.50EUR + Tva. Cod confirmare: " + (randomNumber.nextInt(max - min + 1) + min);
-                    ContentValues values = new ContentValues();
-                    values.put("address", "7479");
-                    values.put("body", linie);
-                    getContentResolver().insert(Uri.parse("content://sms/sent"), values);
-                    values.put("body", text);
-                    getContentResolver().insert(Uri.parse("content://sms/inbox"), values);
+                            ". Cost total:0.50EUR + Tva. Cod confirmare: " + (randomNumber.nextInt(max - min + 1) + min) + ".";
+//                    ContentValues values = new ContentValues();
+//                    values.put("address", "7479");
+//                    values.put("body", linie);
+//                    getContentResolver().insert(Uri.parse("content://sms/sent"), values);
+//                    values.put("body", text);
+//                    getContentResolver().insert(Uri.parse("content://sms/inbox"), values);
+                    startActivity(intent);
                 }
             }
         });
